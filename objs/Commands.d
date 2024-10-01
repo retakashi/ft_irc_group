b31dde1142419721f3403f98b29ca00697908e44
raza shahrukh <shahrukh4062@gmail.com>
2024-10-01 16:10:09 +0000

    feat: just pull the newest version of main.

--- objs/Commands.d
+++
@@ -1,8 +0,0 @@
-objs/Commands.o: command/Commands.cpp command/../Server.hpp \
- command/../ClientData.hpp command/../Server.hpp command/../Channel.hpp \
- command/../CmdResponse.hpp
-command/../Server.hpp:
-command/../ClientData.hpp:
-command/../Server.hpp:
-command/../Channel.hpp:
-command/../CmdResponse.hpp:
--- objs/Send.d
+++
@@ -1,6 +0,0 @@
-objs/Send.o: Send.cpp Server.hpp ClientData.hpp Channel.hpp \
- CmdResponse.hpp
-Server.hpp:
-ClientData.hpp:
-Channel.hpp:
-CmdResponse.hpp:

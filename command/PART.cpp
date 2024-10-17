57ffdbb82ee9b27c74aba540d2c5f4df583f9cc5
Rt3mis10 <117369494+hrinka@users.noreply.github.com>
2024-10-17 17:21:17 +0900

    feat:PART.cpp追加した

--- command/PART.cpp
+++ command/PART.cpp
@@ -2,29 +2,44 @@
 #include "Server.hpp"
 
 void Server::handlePart(const std::string& params, ClientData& client) {
-  std::istringstream iss(params);
-  std::string channelName;
-  iss >> channelName;
+    std::istringstream iss(params);
+    std::string channelName;
+    std::string partMessage;
+    iss >> channelName;
+    std::getline(iss, partMessage);
+    if (!partMessage.empty() && partMessage[0] == ':') {
+        partMessage = partMessage.substr(1);
+    }
 
-  if (channelName.empty()) {
-    sendCmdResponce(ERR_NEEDMOREPARAMS, "PART", client);
-    return;
+    if (channelName.empty()) {
+        sendCmdResponce(ERR_NEEDMOREPARAMS, "PART", client);
+        return;
+    }
+
+    Channel* channel = getChannelByName(channelName);
+    if (!channel) {
+        sendCmdResponce(ERR_NOSUCHCHANNEL, channelName, client);
+        return;
+    }
+
+    if (!channel->isMember(&client)) {
+        sendCmdResponce(ERR_NOTONCHANNEL, channelName, client);
+        return;
+    }
+
+    std::string partMsg = ":" + client.getNickname() + "!" + client.getUsername() + "@" +
+                          "127.0.0.1" + " PART " + channelName + " :" + partMessage + "\r\n";
+    channel->broadcastMessage(partMsg, &client);
+    channel->removeMember(&client);
+
+  // チャンネルが空になった際
+  if (channel->CountMembers() == 0) {
+    std::map<std::string, Channel*>::iterator it = channels_.find(channel->getChannelname());
+    if (it->second->CountMembers() == 0) {
+      std::cout << "Channel " << channel->getChannelname() << " has been deleted." << std::endl;
+      std::map<std::string, Channel*>::iterator erase_it = it;
+      delete it->second;
+      channels_.erase(erase_it);
+    }
   }
-
-  Channel* channel = getChannelByName(channelName);
-  if (!channel) {
-    sendCmdResponce(ERR_NOSUCHCHANNEL, channelName, client);
-    return;
-  }
-
-  if (!channel->isMember(&client)) {
-    sendCmdResponce(ERR_NOTONCHANNEL, channelName, client);
-    return;
-  }
-
-  std::string partMsg = ":" + client.getNickname() + "!" + client.getUsername() + "@" +
-                        getHostname() + " PART :" + channelName;
-  channel->broadcastMessage(partMsg, &client);
-  channel->removeMember(&client);
-  channel->broadcastMessage(client.getNickname() + " has left the channel.", &client);
 }
